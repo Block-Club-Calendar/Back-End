61d25cf9a3221d1564f801a54e2b4ea2f900df3e
D-Fink <dfink4badjuju@gmail.com>
2020-01-31 20:47:30 -0700

    query modules done

---
+++ events/events-model.js
@@ -0,0 +1,16 @@
+//get
+
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    findEvents,
+    findEventById
+}
+
+function findEvents() {
+    return db('events')
+}
+
+function findEventById(id){
+    return db('events').where({id})
+}
