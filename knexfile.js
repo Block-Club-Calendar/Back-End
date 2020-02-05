92e8035a14dba0f5a28cac1e33983179c7ceb009
D-Fink <dfink4badjuju@gmail.com>
2020-02-05 11:27:08 -0700

    added a pool and production to knexfile to try to fix data persistance

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,11 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
   },
   testing: {
     client: 'sqlite3',
@@ -25,4 +30,14 @@ module.exports = {
       directory: './data/migrations',
     },
   },
+  production: {
+    client: "pg", // install this npm package
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: "./data/migrations",
+    },
+    seeds: {
+        directory: "./data/seeds",
+    },
+  }
 };
